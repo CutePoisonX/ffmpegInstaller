93c8e7534c80388bf17df961349e39baf763f2eb
CutePoisonX <CutePoisonXI@gmail.com>
2015-11-11 20:42:24 +0100

    Included license

--- ffmpegInstaller.sh
+++ ffmpegInstaller.sh
@@ -1,10 +1,24 @@
 #!/opt/bin/bash
 
 #  ffmpeg installation script for Synology DiskStations
-#  
 #
-#  Created by CutePoisonX
+#    Copyright 2015 CutePoisonX (CutePoisonXI@gmail.com)
 #
+#
+#    This script is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    ffmpegInstaller is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with ffmpegInstaller.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 RET_COND=-1
 
 endScript ()
